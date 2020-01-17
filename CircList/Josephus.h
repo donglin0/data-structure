bf292c20dda7e8f24155a739c4e56a34bd8f2423
donglin0 <44114683+donglin0@users.noreply.github.com>
2020-01-17 16:44:46 +0800

    6

--- CircList/Josephus.h
+++ CircList/Josephus.h
@@ -4,8 +4,9 @@
 template<class T>
 void Josephus(CircList<T>& Js, int n, int m)
 {
-	CircListNode<T>* p = Js.Locate(1), * pre = NULL;
+	CircListNode<T>* p = Js.Locate(1), * pre = Js.getHead();//初始时pre指向附加头结点
 	int i, j;
+
 	for (i = 0;i < n - 1;i++)//执行n-1次
 	{
 		for (j = 1;j < m;j++)//数m-1个数,j从1开始
